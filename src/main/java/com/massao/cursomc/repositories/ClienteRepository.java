32c4c7b90c91eea23fedc037cd3d8d708ff2be24
Marcos Massao <massaotak@gmail.com>
2020-03-31 20:31:54 -0300

    Cliente, tipoCliente, telefone, endereços

---
+++ src/main/java/com/massao/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.massao.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.massao.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+}
