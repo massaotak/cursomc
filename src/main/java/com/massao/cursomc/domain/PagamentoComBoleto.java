791798431d81a40e0aa40b24566906d323ea0a99
Marcos Massao <massaotak@gmail.com>
2020-04-05 17:27:08 -0300

    EndPoint /pedidos/{id} disponibilizado

--- src/main/java/com/massao/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/massao/cursomc/domain/PagamentoComBoleto.java
@@ -2,13 +2,17 @@ package com.massao.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.massao.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private java.util.Date dataVencimento;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private java.util.Date dataPagamento;
 	
 	public PagamentoComBoleto() {
