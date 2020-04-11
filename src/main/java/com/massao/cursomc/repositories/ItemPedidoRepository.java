f1b0ec1124c3bc9a2b476986ce3e9f685cdf6dec
Marcos Massao <massaotak@gmail.com>
2020-04-11 15:41:33 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/massao/cursomc/repositories/ItemPedidoRepository.java
+++ src/main/java/com/massao/cursomc/repositories/ItemPedidoRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.massao.cursomc.domain.ItemPedido;
-import com.massao.cursomc.domain.Pagamento;
 
 @Repository
 public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
