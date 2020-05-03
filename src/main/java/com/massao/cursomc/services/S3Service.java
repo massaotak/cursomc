a3f61a86158e89532cdfae43d221b15fc28e5306
Marcos Massao <massaotak@gmail.com>
2020-05-03 15:48:08 -0300

    Salvando primeiro arquivo no S3

---
+++ src/main/java/com/massao/cursomc/services/S3Service.java
@@ -0,0 +1,40 @@
+package com.massao.cursomc.services;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@Service
+public class S3Service {
+
+	private Logger log = LoggerFactory.getLogger(S3Service.class);
+
+	@Autowired
+	private AmazonS3 s3Client;
+	
+	@Value("${s3.bucket}")
+	private String bucketName;
+	
+	public void uploadFile(String localFilePath) {
+		try {
+			File file = new File(localFilePath);
+			log.info("Iniciado upload");
+			s3Client.putObject(new PutObjectRequest(bucketName, "teste.jpg", file));
+			log.info("Upload finalizado");
+		}catch(AmazonServiceException e) {
+			log.info("AmazonServiceException: "+e.getErrorMessage());
+			log.info("Status code: "+e.getErrorCode());
+		}catch(AmazonClientException e) {
+			log.info("AmazonClientException: "+e.getMessage());
+		}
+	}
+}
